3cc8cb8b6928182905c8a51f4ff096e5c1c119f7
sneshev <steffal.n2@gmail.com>
2026-04-04 21:16:12 +0300

    ~isValidInput()

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@ int main(int argc, char *argv[]) {
 	t_input input;
 	if (!isValidInput(argc, argv, input)) {
 		cout << "bad input" << endl;
-	} else { cout << "good input" << endl; }
+	}
+
 	Mlx mlx = initializeMlx();
 	Fractal fract(mlx, input);
 
--- src/parsing/validate_input.cpp
+++ src/parsing/validate_input.cpp
@@ -1,36 +1,36 @@
 #include "fract.hpp"
 
-static int getType(str inputType, t_input &input) {
+static bool isValidType(str inputType, t_input &input) {
 	auto typeIter = std::find(validTypes.begin(), validTypes.end(), inputType);
 	if (typeIter == validTypes.end()) {
-		return (-1);
+		return (false);
 	}
 	input.type = static_cast<e_fract_type>(typeIter - validTypes.begin());
-	return (1);
+	return (true);
 }
 
-static int getValues(str cRealString, str cImagString, t_input &input) {
+static bool areValidValues(str cRealString, str cImagString, t_input &input) {
 	float cReal;
 	float cImag;
 	try {
 			cReal = std::stof(cRealString);
 			cImag = std::stof(cImagString);
-		} catch (const std::exception&) {
-			return (-1);
-		}
+	} catch (const std::exception&) {
+		return (false);
+	}
 	input.args.push_back(cReal);
 	input.args.push_back(cImag);
-	return (1);
+	return (true);
 }
 
 bool isValidInput(int argc, char *argv[], t_input &input) {
 	if (argc != 2 && argc != 4) {
 		return (false);
 	}
-	if (getType(argv[1], input) == -1)
+	if (!isValidType(argv[1], input))
 		return (false);
 	if (argc == 4) {
-		if (getValues(argv[2], argv[3], input) == -1)
+		if (!areValidValues(argv[2], argv[3], input))
 			return (false);
 	}
 	return (true);
