3f3613777e5994bb263610f735ffa781e22564b2
sneshev <steffal.n2@gmail.com>
2026-04-06 17:19:46 +0300

    FIRST MANDELBROT

--- src/fractals/Mandelbrot.cpp
+++ src/fractals/Mandelbrot.cpp
@@ -25,49 +25,47 @@ unsigned int Mandelbrot::calcIterations(Fixed c[2], unsigned int maxIterations) 
 	} while (count++ < maxIterations);
 	return (count);
 }
-// 
-// int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
-// {
-    // return (r << 24 | g << 16 | b << 8 | a);
-// }
-// static int getColor(unsigned int iterations, unsigned int max_iterations) {
-    // if (iterations == max_iterations)
-        // return ft_pixel(0, 0, 0, 255); // black, fully opaque
-// 
-    // int r, g, b;
-// 
-    // if (iterations < 6) {
-        // r = 255;
-        // g = iterations * 40;
-        // b = 0;
-    // } else {
-        // r = 255 - iterations * 5;
-        // g = 0;
-        // b = iterations * 10;
-    // }
-    // return ft_pixel(r, g, b, 255);
-// }
+
+int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
+{
+    return (r << 24 | g << 16 | b << 8 | a);
+}
+static int getColor(unsigned int iterations, unsigned int max_iterations) {
+    if (iterations == max_iterations)
+        return ft_pixel(0, 0, 0, 255); // black, fully opaque
+
+    int r, g, b;
+
+    if (iterations < 6) {
+        r = 255;
+        g = iterations * 40;
+        b = 0;
+    } else {
+        r = 255 - iterations * 5;
+        g = 0;
+        b = iterations * 10;
+    }
+    return ft_pixel(r, g, b, 255);
+}
 
 #include <unistd.h>
-// .make x and y ints
-// .make WIDTH and HEIGHT ints
-// .add operator for Fixed * int, Fixed / int, etc.
+
 void Mandelbrot::draw() {
 	cout << "drawing mandelbrot" << endl;
-	// Fixed c[2];
+	Fixed c[2];
 
-	// Fixed xStep = (_xRange[MAX] - _xRange[MIN]) / Fixed(WIDTH);
-	// Fixed yStep = (_yRange[MAX] - _yRange[MIN]) / Fixed(HEIGHT);
-	// for (int y = 0; y < HEIGHT; y++) {
-	// 	c[Y] = _yRange[MIN] + y * yStep;
-	// 	for (int x = 0; x < WIDTH; x++) {
-	// 		c[X] = _xRange[MIN] + x * xStep;
-	// 		unsigned int iterations = calcIterations(c, _maxIterations);
-	// 		uint32_t pixelColor = getColor(iterations, _maxIterations);
-	// 		mlx_put_pixel(getImg(), x.toInt(), y.toInt(), pixelColor);
-	// 		// cout << "putting at " << x << y << endl;
-	// 	}
-	// }
+	Fixed xStep = (_xRange[MAX] - _xRange[MIN]) / WIDTH;
+	Fixed yStep = (_yRange[MAX] - _yRange[MIN]) / HEIGHT;
+	for (int y = 0; y < HEIGHT; y++) {
+		c[Y] = _yRange[MIN] + yStep * y;
+		for (int x = 0; x < WIDTH; x++) {
+			c[X] = _xRange[MIN] + xStep * x;
+			unsigned int iterations = calcIterations(c, _maxIterations);
+			uint32_t pixelColor = getColor(iterations, _maxIterations);
+			mlx_put_pixel(getImg(), x, y, pixelColor);
+			// cout << "putting at " << x << y << endl;
+		}
+	}
 }
 
 /*
