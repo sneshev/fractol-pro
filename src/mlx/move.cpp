d16fbac460384f2195cc45c7b7bc5888e87a179c
sneshev <steffal.n2@gmail.com>
2026-04-07 19:28:25 +0300

    fixed bugs in mlx/move.cpp (there are still bugs if you change width/height)

--- src/mlx/move.cpp
+++ src/mlx/move.cpp
@@ -2,14 +2,14 @@
 
 void move_center(Fractal &fract, Fixed rawX, Fixed rawY) {
 	Fixed xRangeHalf = fract.getXRangeHalf();
-	Fixed yRangeHalf = fract.getXRangeHalf();
+	Fixed yRangeHalf = fract.getYRangeHalf();
 
 	Fixed newXCenter = fract.getXMin() + ((rawX / width)  * fract.getXRange());
 	Fixed newYCenter = fract.getYMin() + ((rawY / height) * fract.getYRange());	
 	
 	fract.setXMin(newXCenter - xRangeHalf);
-	fract.setYMin(newYCenter - xRangeHalf);
-	fract.setXMax(newXCenter + yRangeHalf);
+	fract.setXMax(newXCenter + xRangeHalf);
+	fract.setYMin(newYCenter - yRangeHalf);
 	fract.setYMax(newYCenter + yRangeHalf);
 
 	fract.info();
