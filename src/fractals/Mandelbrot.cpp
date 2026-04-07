d7c4251032f72c1f0ff78cf364d6a975c476d41b
sneshev <steffal.n2@gmail.com>
2026-04-07 17:36:15 +0300

    working on inner loop using __m256i with Vec8i

--- src/fractals/Mandelbrot.cpp
+++ src/fractals/Mandelbrot.cpp
@@ -12,20 +12,33 @@ Mandelbrot::~Mandelbrot() {
 }
 
 Vec8i Mandelbrot::calcIterations(Vec8i cX, Vec8i cY) {
-	Fixed x = 0;
-	Fixed y = 0;
+	Vec8i x(0);
+	Vec8i y(0);
+	Vec8i iterationCount(0);
+    Vec8i one(1);
+	Vec8i four(4);
 
+    __m256i aliveMask = _mm256_set1_epi32(-1); // all pixels start alive
 	for (unsigned int count = 0; count < _maxIterations; count++) {
-		Fixed x2=x*x;
-		Fixed y2=y*y;
-		Fixed newX = x2 - y2 + c[X];
-		y = x*y*2 + c[Y];
+		Vec8i x2=x*x;
+		Vec8i y2=y*y;
+		Vec8i newX = x2 - y2 + cX;
+		y = (x*y).shiftLeft(1) + cY;
 		x = newX;
-		
-		if (x2+y2 > 4)
-			return (count);
+        __m256i notEscaped = _mm256_cmpgt_epi32(four.v, (x2 + y2).v);
+		aliveMask = _mm256_and_si256(aliveMask, notEscaped);
+		if (!_mm256_movemask_epi8(aliveMask))
+            break;
+		iterationCount.v = _mm256_add_epi32(
+            iterationCount.v,
+            _mm256_and_si256(one.v, aliveMask)
+        );
+		Vec8i newX = x2 - y2 + cX;
+        Vec8i newY = (x * y).shiftLeft(1) + cY;
+		Vec8i newX = x2 - y2 + cX;
+        Vec8i newY = (x * y).shiftLeft(1) + cY;
 	}
-	return (_maxIterations);
+	return (iterationCount);
 }
 
 void Mandelbrot::putPixel(unsigned int iterations, unsigned int pixelIndex) {
